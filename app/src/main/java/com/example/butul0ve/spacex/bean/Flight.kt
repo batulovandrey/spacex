ecd0a60590817758024260b2d7c3c131a8be7219
butul0ve <batulov.andrey.89@gmail.com>
2018-01-20 13:13:37 +0300

    added beans

---
+++ app/src/main/java/com/example/butul0ve/spacex/bean/Flight.kt
@@ -0,0 +1,11 @@
+package com.example.butul0ve.spacex.bean
+
+/**
+ * Created by butul0ve on 20.01.18.
+ */
+
+class Flight (private var flightNumber: Int,
+              private var rocket: Rocket,
+              private var launchDate: String,
+              private var links: Links,
+              private var details: String)
---
+++ app/src/main/java/com/example/butul0ve/spacex/bean/Links.kt
@@ -0,0 +1,7 @@
+package com.example.butul0ve.spacex.bean
+
+/**
+ * Created by butul0ve on 20.01.18.
+ */
+
+data class Links(private var missionPath: String)
