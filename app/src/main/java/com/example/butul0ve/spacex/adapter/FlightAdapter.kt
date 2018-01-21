c0d2723242e7fbb7f6ca0c8bc4fda4fb49b597a6
butul0ve <batulov.andrey.89@gmail.com>
2018-01-21 12:31:54 +0300

    minor improvements: added article_button and flight_number_text_view

--- app/src/main/java/com/example/butul0ve/spacex/adapter/FlightAdapter.kt
+++ app/src/main/java/com/example/butul0ve/spacex/adapter/FlightAdapter.kt
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import com.example.butul0ve.spacex.R
@@ -11,6 +12,9 @@ import com.example.butul0ve.spacex.bean.Flight
 import com.squareup.picasso.Picasso
 import java.text.SimpleDateFormat
 import java.util.*
+import android.support.v4.content.ContextCompat.startActivity
+import android.content.Intent
+import android.net.Uri
 
 /**
  * Created by butul0ve on 20.01.18.
@@ -26,6 +30,7 @@ class FlightAdapter(private val mFlights: List<Flight>) : RecyclerView.Adapter<F
     override fun onBindViewHolder(holder: FlightViewHolder, position: Int) {
         val flight = mFlights[position]
         holder.mRocketNameTextView.text = flight.rocket.name
+        holder.mFlightNumberTextView.text = flight.flightNumber.toString()
         val date = Date(flight.launchDate.toLong() * 1000)
         val launchDate = SimpleDateFormat("dd MMMM y, HH:mm:ss", Locale.ENGLISH)
         Picasso.with(holder.itemView.context)
@@ -34,6 +39,12 @@ class FlightAdapter(private val mFlights: List<Flight>) : RecyclerView.Adapter<F
                 .into(holder.mMissionPatchImageView)
         holder.mLaunchDateTextView.text = launchDate.format(date)
         holder.mDetailTextView.text = flight.details
+        holder.mArticleButton.setOnClickListener({
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(flight.links.articleLink)
+            startActivity(holder.itemView.context, intent, null)
+        })
+
     }
 
     override fun getItemCount(): Int {
@@ -43,8 +54,10 @@ class FlightAdapter(private val mFlights: List<Flight>) : RecyclerView.Adapter<F
     class FlightViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         var mRocketNameTextView: TextView = itemView.findViewById(R.id.rocket_name_text_view)
+        var mFlightNumberTextView: TextView = itemView.findViewById(R.id.flight_number_text_view)
         var mLaunchDateTextView: TextView = itemView.findViewById(R.id.launch_date_text_view)
         var mMissionPatchImageView: ImageView = itemView.findViewById(R.id.mission_patch_image_view)
         var mDetailTextView: TextView = itemView.findViewById(R.id.detail_text_view)
+        var mArticleButton: Button = itemView.findViewById(R.id.article_button)
     }
 }
