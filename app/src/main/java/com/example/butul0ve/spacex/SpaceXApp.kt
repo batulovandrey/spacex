32463bf1c7375251bbc09699310bb6350248ddad
Andrey Batulov <batulov.andrey.89@gmail.com>
2018-10-08 23:26:30 +0300

    deleted background service

--- app/src/main/java/com/example/butul0ve/spacex/SpaceXApp.kt
+++ app/src/main/java/com/example/butul0ve/spacex/SpaceXApp.kt
@@ -1,17 +1,12 @@
 package com.example.butul0ve.spacex
 
 import android.app.Application
-import android.content.Intent
 import com.bumptech.glide.request.target.ViewTarget
-import com.example.butul0ve.spacex.service.BackgroundNetworkService
 
 class SpaceXApp: Application() {
 
     override fun onCreate() {
         super.onCreate()
         ViewTarget.setTagId(R.id.glide_tag)
-
-        val intent = Intent(this, BackgroundNetworkService::class.java)
-        startService(intent)
     }
 }
