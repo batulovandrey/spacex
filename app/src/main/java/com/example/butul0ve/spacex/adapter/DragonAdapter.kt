09b40b8ee181bd44cb681aa6f4a85a14d1cc99c0
Andrey Batulov <batulov.andrey.89@gmail.com>
2018-12-11 15:06:34 +0300

    added the opening of Wikipedia when click on the button of a dragon

--- app/src/main/java/com/example/butul0ve/spacex/adapter/DragonAdapter.kt
+++ app/src/main/java/com/example/butul0ve/spacex/adapter/DragonAdapter.kt
@@ -1,9 +1,12 @@
 package com.example.butul0ve.spacex.adapter
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.butul0ve.spacex.R
@@ -33,6 +36,11 @@ class DragonAdapter(private val dragons: List<Dragon>): RecyclerView.Adapter<Dra
             itemView.findViewById<TextView>(R.id.dragon_active_text_view).text = dragon.isActive.toString()
             itemView.findViewById<TextView>(R.id.dragon_description_text_view).text = dragon.description
             itemView.findViewById<TextView>(R.id.first_flight_text_view).text = dragon.firstFlight
+            itemView.findViewById<Button>(R.id.open_wiki_button).setOnClickListener {
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse(dragon.wiki)
+                startActivity(itemView.context, intent, null)
+            }
 
             if (dragon.images.isNotEmpty()) {
                 val tableLayout = TableLayout(itemView.context)
