0cbaa30d6500abfcd6f481c0a17f3cd6e345717e
Andrey Batulov <batulov.andrey.89@gmail.com>
2018-12-11 17:10:38 +0300

    removed list of fragments from PagerAdapter

--- app/src/main/java/com/example/butul0ve/spacex/adapter/SpaceXPagerAdapter.kt
+++ app/src/main/java/com/example/butul0ve/spacex/adapter/SpaceXPagerAdapter.kt
@@ -6,23 +6,18 @@ import androidx.fragment.app.FragmentPagerAdapter
 import com.example.butul0ve.spacex.mvp.fragment.DragonsFragment
 import com.example.butul0ve.spacex.mvp.fragment.MainFragment
 import com.example.butul0ve.spacex.mvp.fragment.UpcomingFragment
-import com.example.butul0ve.spacex.ui.BaseFragment
 
 class SpaceXPagerAdapter(manager: FragmentManager): FragmentPagerAdapter(manager) {
 
-    private val fragments = ArrayList<BaseFragment>()
-
-    init {
-        fragments.add(UpcomingFragment())
-        fragments.add(MainFragment())
-        fragments.add(DragonsFragment())
-    }
-
     override fun getItem(position: Int): Fragment {
-        return fragments[position]
+        return when(position) {
+            0 -> UpcomingFragment()
+            1 -> MainFragment()
+            else -> DragonsFragment()
+        }
     }
 
     override fun getCount(): Int {
-        return fragments.size
+        return 3
     }
 }
