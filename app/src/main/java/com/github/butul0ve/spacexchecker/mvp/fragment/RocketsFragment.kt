a072302edf3af39f9b894e5c464ce4705042ca65
Andrey Batulov <batulov.andrey.89@gmail.com>
2018-12-17 12:08:20 +0300

    removed extra layout files

--- app/src/main/java/com/github/butul0ve/spacexchecker/mvp/fragment/RocketsFragment.kt
+++ app/src/main/java/com/github/butul0ve/spacexchecker/mvp/fragment/RocketsFragment.kt
@@ -2,15 +2,10 @@ package com.github.butul0ve.spacexchecker.mvp.fragment
 
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
-import com.github.butul0ve.spacexchecker.R
 import com.github.butul0ve.spacexchecker.SpaceXApp
 import com.github.butul0ve.spacexchecker.adapter.RocketAdapter
 import com.github.butul0ve.spacexchecker.mvp.interactor.RocketsMvpInteractor
@@ -20,10 +15,6 @@ import com.github.butul0ve.spacexchecker.ui.BaseFragment
 import javax.inject.Inject
 
 class RocketsFragment: BaseFragment(), RocketsView, SwipeRefreshLayout.OnRefreshListener {
-
-    private lateinit var rocketsRecyclerView: RecyclerView
-    private lateinit var tryAgainButton: Button
-    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
 
     @Inject
     lateinit var intercator: RocketsMvpInteractor
@@ -39,26 +30,14 @@ class RocketsFragment: BaseFragment(), RocketsView, SwipeRefreshLayout.OnRefresh
         SpaceXApp.netComponent.inject(this)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.rockets_fragment, container, false)
-        rocketsRecyclerView = view.findViewById(R.id.rockets_recycler)
-        swipeRefreshLayout = view.findViewById(R.id.swipe_refresh_layout)
-        swipeRefreshLayout.setOnRefreshListener(this)
-        tryAgainButton = view.findViewById(R.id.try_again_button)
-        tryAgainButton.setOnClickListener { rocketsPresenter.getData() }
-        return view
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary,
-                android.R.color.holo_green_dark,
-                android.R.color.holo_orange_dark,
-                android.R.color.holo_blue_dark)
+        swipeRefreshLayout.setOnRefreshListener(this)
+        tryAgainButton.setOnClickListener { rocketsPresenter.getData() }
     }
 
     override fun setAdapter(adapter: RocketAdapter) {
-        rocketsRecyclerView.adapter = adapter
+        recyclerView.adapter = adapter
     }
 
     override fun showProgressBar() {
--- app/src/main/java/com/github/butul0ve/spacexchecker/mvp/fragment/UpcomingFragment.kt
+++ app/src/main/java/com/github/butul0ve/spacexchecker/mvp/fragment/UpcomingFragment.kt
@@ -2,16 +2,11 @@ package com.github.butul0ve.spacexchecker.mvp.fragment
 
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
-import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
-import com.github.butul0ve.spacexchecker.R
 import com.github.butul0ve.spacexchecker.SpaceXApp
 import com.github.butul0ve.spacexchecker.adapter.LaunchesAdapter
 import com.github.butul0ve.spacexchecker.mvp.interactor.UpcomingMvpInteractor
@@ -21,10 +16,6 @@ import com.github.butul0ve.spacexchecker.ui.BaseFragment
 import javax.inject.Inject
 
 class UpcomingFragment : BaseFragment(), UpcomingView, SwipeRefreshLayout.OnRefreshListener {
-
-    private lateinit var dragonsRecycler: RecyclerView
-    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
-    private lateinit var tryAgainButton: Button
 
     private var toast: Toast? = null
 
@@ -42,26 +33,14 @@ class UpcomingFragment : BaseFragment(), UpcomingView, SwipeRefreshLayout.OnRefr
         SpaceXApp.netComponent.inject(this)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.base_fragment, container, false)
-        dragonsRecycler = view.findViewById(R.id.recycler_view)
-        swipeRefreshLayout = view.findViewById(R.id.swipe_refresh_layout)
-        swipeRefreshLayout.setOnRefreshListener(this)
-        tryAgainButton = view.findViewById(R.id.try_again_button)
-        tryAgainButton.setOnClickListener { upcomingPresenter.getData() }
-        return view
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary,
-                android.R.color.holo_green_dark,
-                android.R.color.holo_orange_dark,
-                android.R.color.holo_blue_dark)
+        swipeRefreshLayout.setOnRefreshListener(this)
+        tryAgainButton.setOnClickListener { upcomingPresenter.getData() }
     }
 
     override fun setAdapter(adapter: LaunchesAdapter) {
-        dragonsRecycler.adapter = adapter
+        recyclerView.adapter = adapter
     }
 
     override fun showProgressBar() {
