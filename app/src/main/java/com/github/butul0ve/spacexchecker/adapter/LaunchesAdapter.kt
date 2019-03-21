80aa5b72632a961ebeb7a3dd1c300554ab5320bc
Andrey Batulov <batulov.andrey.89@gmail.com>
2019-03-21 12:49:51 +0300

    The ViewHolders from LaunchesAdapter move into separate classes

--- app/src/main/java/com/github/butul0ve/spacexchecker/adapter/LaunchesAdapter.kt
+++ app/src/main/java/com/github/butul0ve/spacexchecker/adapter/LaunchesAdapter.kt
@@ -1,19 +1,13 @@
 package com.github.butul0ve.spacexchecker.adapter
 
-import android.content.Intent
-import android.net.Uri
 import android.os.CountDownTimer
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.github.butul0ve.spacexchecker.R
+import com.github.butul0ve.spacexchecker.adapter.viewholder.PastLaunchesViewHolder
+import com.github.butul0ve.spacexchecker.adapter.viewholder.UpcomingLaunchesViewHolder
 import com.github.butul0ve.spacexchecker.db.model.Launch
-import com.squareup.picasso.Picasso
 import org.threeten.bp.Instant
 import org.threeten.bp.LocalDateTime
 import org.threeten.bp.ZoneId
@@ -108,139 +102,5 @@ class LaunchesAdapter(private val launches: List<Launch>, private val clickListe
         this.launches.clear()
         this.launches.addAll(launches)
         notifyDataSetChanged()
-    }
-
-    inner class PastLaunchesViewHolder(itemView: View, private val listener: LaunchesClickListener) :
-            RecyclerView.ViewHolder(itemView), View.OnClickListener {
-
-        init {
-            itemView.setOnClickListener(this)
-        }
-
-        override fun onClick(view: View?) {
-            /*
-                onClick item not used yet
-             */
-        }
-
-        fun bind(pastLaunch: Launch, dateTime: ZonedDateTime) {
-            itemView.findViewById<TextView>(R.id.rocket_name_text_view).text = pastLaunch.rocket.name
-            itemView.findViewById<TextView>(R.id.flight_number_text_view).text = pastLaunch.flightNumber.toString()
-            itemView.findViewById<TextView>(R.id.mission_name_text_view).text = pastLaunch.missionName
-
-            Picasso.get()
-                    .load(pastLaunch.links.missionPathSmall)
-                    .into(itemView.findViewById<ImageView>(R.id.mission_patch_image_view))
-
-            itemView.findViewById<TextView>(R.id.launch_date_text_view).text =
-                    dateTime.format(DateTimeFormatter.ofPattern("dd MMMM y, HH:mm:ss"))
-            itemView.findViewById<TextView>(R.id.detail_text_view).text = pastLaunch.details
-
-            val articleButton = itemView.findViewById<ImageButton>(R.id.article_button)
-            if (pastLaunch.links.articleLink.isNullOrEmpty()) {
-                articleButton.visibility = View.GONE
-            } else {
-                articleButton.setOnClickListener {
-                    val intent = Intent(Intent.ACTION_VIEW)
-                    intent.data = Uri.parse(pastLaunch.links.articleLink)
-                    startActivity(itemView.context, intent, null)
-                }
-            }
-
-            val youtubeButton = itemView.findViewById<ImageButton>(R.id.youtube_button)
-            if (pastLaunch.links.videoLink.isNullOrEmpty()) {
-                youtubeButton.visibility = View.GONE
-            } else {
-                youtubeButton.setOnClickListener {
-                    listener.onYoutubeButtonClick(layoutPosition)
-                }
-            }
-
-            val redditCampaignButton = itemView.findViewById<ImageButton>(R.id.reddit_campaign_button)
-            if (pastLaunch.links.redditCampaign.isNullOrEmpty()) {
-                redditCampaignButton.visibility = View.GONE
-            } else {
-                redditCampaignButton.setOnClickListener {
-                    listener.onRedditCampaignButtonClick(layoutPosition)
-                }
-            }
-
-            val redditLaunchButton = itemView.findViewById<ImageButton>(R.id.reddit_launch_button)
-            if (pastLaunch.links.redditLaunch.isNullOrEmpty()) {
-                redditLaunchButton.visibility = View.GONE
-            } else {
-                redditLaunchButton.setOnClickListener {
-                    listener.onRedditLaunchButtonClick(layoutPosition)
-                }
-            }
-        }
-    }
-
-    inner class UpcomingLaunchesViewHolder(itemView: View, private val listener: LaunchesClickListener) :
-            RecyclerView.ViewHolder(itemView), View.OnClickListener {
-
-        var countDownTimer: CountDownTimer? = null
-        val launchDateTextView: TextView = itemView.findViewById(R.id.launch_date_text_view)
-
-        init {
-            itemView.setOnClickListener(this)
-        }
-
-        override fun onClick(v: View?) {
-            /*
-                onClick item not used yet
-             */
-        }
-
-        fun bind(upcomingLaunch: Launch, dateTime: ZonedDateTime) {
-            itemView.findViewById<TextView>(R.id.rocket_name_text_view).text = upcomingLaunch.rocket.name
-            itemView.findViewById<TextView>(R.id.flight_number_text_view).text = upcomingLaunch.flightNumber.toString()
-            itemView.findViewById<TextView>(R.id.mission_name_text_view).text = upcomingLaunch.missionName
-
-            Picasso.get()
-                    .load(upcomingLaunch.links.missionPathSmall)
-                    .into(itemView.findViewById<ImageView>(R.id.mission_patch_image_view))
-
-            itemView.findViewById<TextView>(R.id.detail_text_view).text = upcomingLaunch.details
-            launchDateTextView.text = dateTime.format(DateTimeFormatter.ofPattern("dd MMMM y, HH:mm:ss"))
-
-            val articleButton = itemView.findViewById<ImageButton>(R.id.article_button)
-            if (upcomingLaunch.links.articleLink.isNullOrEmpty()) {
-                articleButton.visibility = View.GONE
-            } else {
-                articleButton.setOnClickListener {
-                    val intent = Intent(Intent.ACTION_VIEW)
-                    intent.data = Uri.parse(upcomingLaunch.links.articleLink)
-                    startActivity(itemView.context, intent, null)
-                }
-            }
-
-            val youtubeButton = itemView.findViewById<ImageButton>(R.id.youtube_button)
-            if (upcomingLaunch.links.videoLink.isNullOrEmpty()) {
-                youtubeButton.visibility = View.GONE
-            } else {
-                youtubeButton.setOnClickListener {
-                    listener.onYoutubeButtonClick(layoutPosition)
-                }
-            }
-
-            val redditCampaingButton = itemView.findViewById<ImageButton>(R.id.reddit_campaign_button)
-            if (upcomingLaunch.links.redditCampaign.isNullOrEmpty()) {
-                redditCampaingButton.visibility = View.GONE
-            } else {
-                redditCampaingButton.setOnClickListener {
-                    listener.onRedditCampaignButtonClick(layoutPosition)
-                }
-            }
-
-            val redditLaunchButton = itemView.findViewById<ImageButton>(R.id.reddit_launch_button)
-            if (upcomingLaunch.links.redditLaunch.isNullOrEmpty()) {
-                redditLaunchButton.visibility = View.GONE
-            } else {
-                redditLaunchButton.setOnClickListener {
-                    listener.onRedditLaunchButtonClick(layoutPosition)
-                }
-            }
-        }
     }
 }
