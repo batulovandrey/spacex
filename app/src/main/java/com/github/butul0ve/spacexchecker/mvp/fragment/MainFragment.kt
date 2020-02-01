a53472effab0724267c0e5c708a1c7a583f89622
Andrey Batulov <batulov@gmail.com>
2020-02-01 15:13:47 +0300

    upgrade version of some libs

--- app/src/main/java/com/github/butul0ve/spacexchecker/mvp/fragment/MainFragment.kt
+++ app/src/main/java/com/github/butul0ve/spacexchecker/mvp/fragment/MainFragment.kt
@@ -36,7 +36,7 @@ class MainFragment : BaseFragment(), MainView, SwipeRefreshLayout.OnRefreshListe
     @ProvidePresenter
     fun provideMainPresenter() = MainPresenter(interactor)
 
-    override fun onAttach(context: Context?) {
+    override fun onAttach(context: Context) {
         super.onAttach(context)
         SpaceXApp.netComponent.inject(this)
         try {
